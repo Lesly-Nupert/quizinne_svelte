96cfc6e81902b90819d2e355611640535c25f951
Lesly-Nupert <lesly.nupert@gmail.com>
2024-02-25 16:23:40 +0100

    Ajout background-image décorative dans Page Contact

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,9 +1,9 @@
-<p class="contact_us">
-    Pour tout renseignement, veuillez nous écrire à : <a
-        href="mailto:quizine7@gmail.com">quizine7@gmail.com</a
-    >
-</p>
+<main class="contact">
+    <p class="contact_us">
+        Pour tout renseignement, veuillez nous écrire à : <a
+            href="mailto:quizine7@gmail.com">quizine7@gmail.com</a
+        >
+    </p>
+</main>
 
-<style>
-    
-</style>
+
